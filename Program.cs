c82f9e2283fec3b24dee1e79eec1d690f5729845
GiulsV <valente.giulia93@gmail.com>
2022-10-28 14:25:57 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//array di numeri [2, 6, 7, 5, 3, 9] 
+
+
+//void StampaArray(int[] array): che preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma "[elemento 1, elemento 2, elemento 3, ...]"
+
+
+
+
+//int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro.
+
+
+
+//int[] ElevaArrayAlQuadrato(int[] array): che preso un array di numeri interi, restituisca un nuovo array con tutti gli elementi elevati quadrato.
+//Attenzione: è importante restituire un nuovo array, e non modificare l’array come parametro della funzione!(vedi slide)
+
+
+
+
+//int SommaElementiArray(int[] array): che preso un array di numeri interi, restituisca la somma totale di tutti gli elementi dell’array.
+
+
+
+
